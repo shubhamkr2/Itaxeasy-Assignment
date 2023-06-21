f3c394dc2505ff4c3983202f72a746dcebb456f7
Shubham Kumar <kumarshubham417@gmail.com>
2023-06-21 19:38:55 +0530

    taking form input

--- src/components/AdvanceTaxForm/HufForm.jsx
+++ src/components/AdvanceTaxForm/HufForm.jsx
@@ -1,48 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import styles from "../../styles/AdvanceTax.module.css";
 
-function Huf({ handleChange }) {
+function Huf({ setHufFormData, hufFormData }) {
   const [isDisabled, setIsDisabled] = useState(true);
 
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setHufFormData({ ...hufFormData, [name]: value });
+  }
+  console.log(hufFormData);
   return (
     <>
       <div>
         <label>Whether opting for taxation under Section 115BAC?</label>
-        <select>
-          <option selected="selected" value="Select">
-            Select
-          </option>
+        <select
+          onChange={(e)=>handleChange(e)}
+          name="opting_for_tax"
+          value={hufFormData.opting_for_tax}
+        >
+          <option value="">Select</option>
           <option value="Yes">Yes</option>
           <option value="No">No</option>
         </select>
       </div>
       <div>
         <label>Net Taxable Income</label>
-        <input type="number" onChange={handleChange} disabled={isDisabled} />
+        <input type="number" disabled={isDisabled} />
       </div>
       <div className={styles.bg_gray}>
         <label>Income Tax</label>
-        <input type="number" onChange={handleChange} disabled={isDisabled} />
+        <input type="number" disabled={isDisabled} />
       </div>
       <div>
         <label>Surcharge</label>
-        <input type="number" onChange={handleChange} disabled={isDisabled} />
+        <input type="number" disabled={isDisabled} />
       </div>
       <div>
         <label>Health and Education Cess</label>
-        <input type="number" onChange={handleChange} disabled={isDisabled} />
+        <input type="number" disabled={isDisabled} />
       </div>
       <div className={styles.bg_gray}>
         <label className={styles.bg_gray}>Total Tax Liability</label>
-        <input type="number" onChange={handleChange} disabled={isDisabled} />
+        <input type="number" disabled={isDisabled} />
       </div>
       <div>
         <label>Relief</label>
-        <input type="number" value={""} />
+        <input
+          type="number"
+          name="relief"
+          value={hufFormData.relief}
+          onChange={(e)=>handleChange(e)}
+        />
       </div>
       <div className={styles.bg_gray}>
         <label>TDS/TCS/MAT (AMT) Credit Utilized</label>
-        <input type="number" value={""} />
+        <input
+          type="number"
+          name="credit_utilized"
+          value={hufFormData.credit_utilized}
+          onChange={(e)=>handleChange(e)}
+        />
       </div>
       <div>
         <label>Assessed Tax</label>
